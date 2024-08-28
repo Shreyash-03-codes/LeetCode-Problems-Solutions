6dfd624064789633a7830d4858999accbb298170
Shreyash-03-codes <guravshreyash2005@gmail.com>
2024-08-28 20:59:30 +0530

    Time: 14 ms (23.73%), Space: 44.6 MB (45.66%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void duplicateZeros(int[] nums) {
+        
+        for(int i=0;i<nums.length;i++){
+            
+            if(nums[i]==0 &&i+1<nums.length){
+                for(int j=nums.length-1;j>i+1;j--){
+                    nums[j]=nums[j-1];
+                }
+                nums[i+1]=0;
+                i++;
+            }
+        }
+    }
+}
