c277bca18b133dc3990b9cdc8f886f782cd179e9
Shreyash-03-codes <guravshreyash2005@gmail.com>
2024-11-20 20:15:58 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (96.01%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,26 +2,27 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
         for(int[] nums:matrix){
-            if(nums[0]<= target && target<=nums[nums.length-1]){
-                int start=0;
-                int last=nums.length-1;
-                
-                while(start<=last){
-                    int mid=(start+last)/2;
-                    
-                    if(nums[mid]==target){
-                        return true;
-                    }
-                    
-                    else if(target<nums[mid]){
-                        last=mid-1;
-                    }
-                    else{
-                        start=mid+1;
-                    }
-                }
-            }
+            int start=0;
+            int last=nums.length-1;
             
+            while(start<=last){
+                int mid=(start+last)/2;
+                
+                if(nums[mid]==target){
+                    return true;
+                    
+                }
+                
+                if(target<nums[mid]){
+                    last=mid-1;
+                }
+                
+                if(target>nums[mid]){
+                    start=mid+1;
+                }
+                
+                
+            }
         }
         return false;
     }
