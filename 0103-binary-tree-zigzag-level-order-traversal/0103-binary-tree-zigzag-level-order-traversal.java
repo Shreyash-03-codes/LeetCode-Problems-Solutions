a3a9c00437d1c4dbe2a7258c1eae62f4e27ead3f
Shreyash-03-codes <guravshreyash2005@gmail.com>
2024-11-30 21:17:24 +0530

    Time: 1 ms (71.09%), Space: 42.2 MB (28.77%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,59 +16,47 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
-        ArrayList<List<Integer>> result=new ArrayList<>();
-        
+        Deque<TreeNode> que=new LinkedList<>();
+        List<List<Integer>> result=new ArrayList<>();
         if(root==null){
             return result;
         }
-        
-        Deque<TreeNode> queue=new ArrayDeque<>();
-        
-        queue.offer(root);
-        
-        boolean reverse=false;
-        
-        while(!queue.isEmpty()){
-            
+        que.add(root);
+        boolean flag=true;
+        while(!que.isEmpty()){
+            int n=que.size();
             ArrayList<Integer> list=new ArrayList<>();
-            
-            int size=queue.size();
-            
-            for(int i=0;i<size;i++){
-                
-                if(!reverse){
-                    TreeNode node=queue.pollFirst();
+            for(int i=0;i<n;i++){
+                if(flag){
+                    TreeNode node=que.removeFirst();
+                    list.add(node.val);
                     if(node.left!=null){
-                        queue.addLast(node.left);
+                        que.addLast(node.left);
+                        
                     }
                     if(node.right!=null){
-                        queue.addLast(node.right);
+                        que.addLast(node.right);
+                        
                     }
-                    
-                    list.add(node.val);
                     
                 }
                 else{
                     
-                    TreeNode node=queue.pollLast();
-                    
+                    TreeNode node=que.removeLast();
+                    list.add(node.val);
                     if(node.right!=null){
-                        queue.addFirst(node.right);
+                        que.addFirst(node.right);
+                        
                     }
                     if(node.left!=null){
-                        queue.addFirst(node.left);
+                        que.addFirst(node.left);
+                        
                     }
-                    
-                    list.add(node.val);
-                    
                 }
                 
             }
-            
             result.add(list);
-            
-            reverse=!reverse;
-            
+            flag=!flag;
         }
         return result;
     }
