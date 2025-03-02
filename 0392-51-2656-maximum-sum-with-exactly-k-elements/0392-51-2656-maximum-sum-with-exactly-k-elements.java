1ce13b3f0c50dbc476e9aac7264ca4a338934717
Shreyash-03-codes <guravshreyash2005@gmail.com>
2025-03-02 20:35:13 +0530

    Time: 11 ms, Memory: 44.9 MB - LeetHub

---
+++ 0392-51-2656-maximum-sum-with-exactly-k-elements/0392-51-2656-maximum-sum-with-exactly-k-elements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximizeSum(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(new Comparator<Integer>(){
+            public int compare(Integer a,Integer b){
+                return b-a;
+            }
+        });
+        int sum=0;
+        for(int e:nums){
+            pq.add(e);
+        }
+        while(k!=0){
+            int e=pq.remove();
+            sum+=e;
+            pq.add(e+1);
+            k--;
+        }
+        return sum;
+    }
+}
