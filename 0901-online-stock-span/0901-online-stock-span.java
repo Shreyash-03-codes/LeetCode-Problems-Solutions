cf2d6914363a100e997301d76da8888404a3949b
Shreyash-03-codes <guravshreyash2005@gmail.com>
2025-05-13 18:15:12 +0530

    Time: 423 ms (9.94%), Space: 55.7 MB (29.83%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,38 +1,20 @@
 class StockSpanner {
 
-    Stack<int[]> stack;
+    ArrayList<Integer> list;
+
     public StockSpanner() {
-        stack=new Stack<>();
+        list=new ArrayList<>();
     }
     
     public int next(int price) {
-
-        if(stack.isEmpty()){
-            stack.add(new int[]{price,1});
-            return 1;
+        int count=1;
+        int i=list.size()-1;
+        while(i>=0 && list.get(i)<=price){
+            i--;
+            count++;
         }
-        
-            int count=1;
-            while(!stack.isEmpty() && price>=stack.peek()[0]){
-                count+=stack.pop()[1];
-            }
-            stack.push(new int[]{price,count});
-            return count;
-        
-
-        // stack.push(price);
-        // int count=0;
-        // Queue<Integer> que=new LinkedList<>();
-        // while(!stack.isEmpty() && price>=stack.peek()){
-        //     count++;
-        //     int n=stack.pop();
-        //     que.add(n);
-        // }
-
-        // while(!que.isEmpty()){
-        //     stack.push(que.remove());
-        // }
-        // return count;
+        list.add(price);
+        return count;
     }
 }
 
