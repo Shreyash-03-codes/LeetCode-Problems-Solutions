851ffe63c49b3bedd8e2721985b4d1ed1f3594a1
Shreyash-03-codes <guravshreyash2005@gmail.com>
2025-05-22 20:19:15 +0530

    Time: 11 ms (76.72%), Space: 47.1 MB (5.28%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -2,12 +2,11 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         Stack<Integer> stack=new Stack<>();
         int[]result=new int[nums.length];
-        boolean[]visited=new boolean[nums.length];
         
         for(int i=0;i<nums.length;i++){
             while(!stack.isEmpty() && nums[stack.peek()]<nums[i]){
                 result[stack.peek()]=nums[i];
-                visited[stack.peek()]=true;
+            
                 stack.pop();
             }
 
@@ -16,16 +15,16 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             while(!stack.isEmpty() && nums[stack.peek()]<nums[i]){
                 result[stack.peek()]=nums[i];
-                visited[stack.peek()]=true;
+              
                 stack.pop();
             }
 
            
         }
-        for(int i=0;i<nums.length;i++){
-            if(!visited[i]){
-                result[i]=-1;
-            }
+        while(!stack.isEmpty()){
+            
+            result[stack.pop()]=-1;
+
         }
         return result;
     }
