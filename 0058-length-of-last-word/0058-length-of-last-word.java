b412b61c2ea21e9c56218589b6e5136803feba57
Shreyash-03-codes <guravshreyash2005@gmail.com>
2025-07-06 20:51:02 +0530

    Time: 2 ms (12.35%), Space: 41.9 MB (31.95%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-
-        String[] words=s.split("\\W");
-        
+        String[]words=s.trim().split("\\s+");
         return words[words.length-1].length();
     }
 }
